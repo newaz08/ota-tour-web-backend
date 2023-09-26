3d8a84ea3e04f6560c9a165075321e973cfbd3e6
Shahed-DG99045 <norul.shahed@digigate360.com>
2023-09-26 14:22:38 +0600

    Change Swagger name and Description,Change method name in TourPackageContentService and delete TourPackageContentMapper

--- src/main/java/com/technonext/ota/b2c/tour/mapper/LocationMapper.java
+++ src/main/java/com/technonext/ota/b2c/tour/mapper/LocationMapper.java
@@ -3,10 +3,10 @@ package com.technonext.ota.b2c.tour.mapper;
 import com.technonext.ota.b2c.tour.dto.response.LocationResponse;
 import com.technonext.ota.b2c.tour.model.entity.Location;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+
 
 import java.util.List;
-import java.util.Map;
+
 
 @Mapper(componentModel = "spring")
 public interface LocationMapper {
--- src/main/java/com/technonext/ota/b2c/tour/repository/LocationRepository.java
+++ src/main/java/com/technonext/ota/b2c/tour/repository/LocationRepository.java
@@ -3,10 +3,9 @@ package com.technonext.ota.b2c.tour.repository;
 import com.technonext.ota.b2c.tour.model.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
-import java.util.Map;
 
 public interface LocationRepository extends JpaRepository<Location, String> {
 
