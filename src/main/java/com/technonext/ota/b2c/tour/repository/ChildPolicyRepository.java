33a1a09ce6f6f07345a7286576067299867d8a3f
Shahed-DG99045 <norul.shahed@digigate360.com>
2023-10-02 17:55:28 +0600

    Commit ChildPolicyRepository

--- src/main/java/com/technonext/ota/b2c/tour/repository/ChildPolicyRepository.java
+++ src/main/java/com/technonext/ota/b2c/tour/repository/ChildPolicyRepository.java
@@ -1,7 +1,6 @@
 package com.technonext.ota.b2c.tour.repository;
 
 import com.technonext.ota.b2c.tour.dto.response.ChildPolicyResponse;
-
 import com.technonext.ota.b2c.tour.model.entity.ChildPolicy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
