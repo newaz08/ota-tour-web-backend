b9d70a46aaec4b4daa9a82c3aca59e3d185d1ec3
Shahed-DG99045 <norul.shahed@digigate360.com>
2023-10-11 12:52:58 +0600

    Concat Country Name with Location Name in Location Repository

--- src/main/java/com/technonext/ota/b2c/tour/repository/LocationRepository.java
+++ src/main/java/com/technonext/ota/b2c/tour/repository/LocationRepository.java
@@ -1,5 +1,6 @@
 package com.technonext.ota.b2c.tour.repository;
 
+import com.technonext.ota.b2c.tour.dto.response.LocationProjection;
 import com.technonext.ota.b2c.tour.model.entity.Location;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,8 +9,9 @@ import org.springframework.data.jpa.repository.Query;
 
 
 public interface LocationRepository extends JpaRepository<Location, String> {
-
-    @Query(value = "SELECT * FROM tour.Location as loc \n" +
-            "Where loc.LocationName LIKE '%'+:locationName+'%'", nativeQuery = true)
-    Page<Location> findByLocationName(String locationName, Pageable pageable);
+    @Query(value = """
+            SELECT loc.Id,concat(loc.LocationName,',',tc.Name) locationName FROM tour.Location as loc
+            left join tour.TourCountry as tc on tc.Id=loc.CountryId
+            Where loc.LocationName LIKE '%'+:locationName+'%'""", nativeQuery = true)
+    Page<LocationProjection> findByLocationName(String locationName, Pageable pageable);
 }
